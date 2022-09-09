cde0182d7116ed7182896292c73e215f64d028c4
Aniket-x-code <aniket.209301578@muj.manipal.edu>
2022-09-10 01:23:11 +0530

    done

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require ("express");
 const app = express();
 const dotenv = require("dotenv")
+const morgan = require("morgan")
 
 
 dotenv.config({path: './views/config.env'})
 
 const PORT = process.env.PORT;
+
+// log req
+
+app.use(morgan('tiny'))
 
 app.get("/", (req, res)=>{
     res.send("welcome to local sever")
