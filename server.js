ef1d4f2e5b4d4aaf4b7828625ef71e49db369152
Aniket-x-code <aniket.209301578@muj.manipal.edu>
2022-09-11 19:53:35 +0530

    adding routes :truck:

--- server.js
+++ server.js
@@ -28,20 +28,8 @@ app.use('/css', express.static(path.resolve(__dirname,"assets/css")))
 app.use('/css', express.static(path.resolve(__dirname,"assets/img")))
 app.use('/css', express.static(path.resolve(__dirname,"assets/js")))
 
-app.get("/", (req, res)=>{
-    res.render('index');
-})
-
-app.get("/add-user", (req, res)=>{
-    res.render('add_user');
-})
-
-
-app.get("/update-user", (req, res)=>{
-    res.render('update_user');
-})
-
-
+// loading routers
+app.use('/',require('./server/routes/router'))
 
 
 app.listen(PORT, ()=>{
---
+++ server/routes/router.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const route = express.Router()
+
+
+
+route.get("/", (req, res)=>{
+    res.render('index');
+})
+
+route.get("/add-user", (req, res)=>{
+    res.render('add_user');
+})
+
+
+route.get("/update-user", (req, res)=>{
+    res.render('update_user');
+})
+
